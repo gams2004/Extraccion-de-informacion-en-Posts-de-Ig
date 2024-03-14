2a6cb9de7634dffabb4b1f7d48afc84fc5d1e693
gams2004 <gabrielmartin2k4@gmail.com>
2024-03-14 13:54:59 -0500

    Importación de librerías y declaración de funciones

--- ExtraccionLambda.py
+++ ExtraccionLambda.py
@@ -1,1 +1,23 @@
+import boto3
+from datetime import datetime, timedelta
+from instascrape import Profile
+from pysentimiento import SentimentAnalysis
 
+def lambda_handler(event, context):
+    return results
+
+def schedule_lambda():
+    print("Lambda exitoso")
+
+
+
+"""
+@misc{perez2021pysentimiento,
+      title={pysentimiento: A Python Toolkit for Opinion Mining and Social NLP tasks}, 
+      author={Juan Manuel Pérez and Mariela Rajngewerc and Juan Carlos Giudici and Damián A. Furman and Franco Luque and Laura Alonso Alemany and María Vanina Martínez},
+      year={2023},
+      eprint={2106.09462},a
+      archivePrefix={arXiv},
+      primaryClass={cs.CL}
+}
+"""
