5906b8fa9f8b967c8c79afdb4e7ca8dc8fc4a113
gams2004 <gabrielmartin2k4@gmail.com>
2024-06-25 20:33:08 -0500

    Obtención de resultado por similitud de Jaccard n-gramas

--- Lambda/open_search/ExtraccionLambdaIgAbierta.py
+++ Lambda/open_search/ExtraccionLambdaIgAbierta.py
@@ -68,6 +68,26 @@ def extract_hashtags_mentions(text):
     }
     
     return result
+
+def jaccard_ngramas(word1, word2, n=2):
+    # Crear los n-gramas para ambas palabras
+    ngrams1 = set([word1[i:i+n] for i in range(len(word1)-n+1)])
+    ngrams2 = set([word2[i:i+n] for i in range(len(word2)-n+1)])
+    
+    # Calcular la intersección y la unión de los conjuntos de n-gramas
+    intersection = len(ngrams1 & ngrams2)
+    union = len(ngrams1 | ngrams2)
+    
+    # Devolver la similitud de Jaccard
+    return intersection / union
+
+def similitud_j(word_list, target_word, n=2):
+    # Calcular la similitud para cada palabra en la lista
+    similarities = [jaccard_ngramas(word, target_word, n) for word in word_list]
+    
+    # Encontrar el índice de la palabra con la mayor similitud
+    max_similarity_index = similarities.index(max(similarities))
+    return max_similarity_index
     
 #Función que extrae los primeros 50 comentarios de un post dado
 def extraccion_comentarios_ig(padre, urls, num_comentarios):
@@ -276,7 +296,11 @@ def lambda_handler(event, context):
         if len(datos) == 0:
             return {"response": "No se encontraron resultados asociados a la búsqueda"}
 
-        return extraer_posts(datos[2].get("latestPosts")[:max_posts], search, 50)
+        #Encontramos el resultado más parecido al término de búsqueda 
+        terms = [item.get("name") for item in datos]
+        indice = similitud_j(terms, search)
+
+        return extraer_posts(datos[indice].get("latestPosts")[:max_posts], search, 50)
     
     except Exception as e:
         return {"response": "Error" + str(e)}
